2fd337b8969993e19849fa5e06e557a27d7c987b
Linda Johnstone <linda.johnstone@myob.com>
2021-01-13 11:08:27 +1100

    Initial commit

---
+++ PrimeNumberCalculatorTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PrimeNumberCalculatorTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
